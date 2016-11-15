1ddd4ee73eed7af15db3a222463923c54a7ce5ed
zyj <642914257@qq.com>
2016-11-15 15:56:02 +0800

    11.15

--- public/index.js
+++ public/index.js
@@ -7,168 +7,213 @@
 //宝贝分类
 var bbfl = document.getElementById("centre_top_centre_g_id");
 var superior = document.getElementById("centre_top_centre_1");
-var caidan =document.getElementById("centre_top_centre_1_cd");
+var caidan = document.getElementById("centre_top_centre_1_cd");
 
-bbfl.onmousemove=function(){
+bbfl.onmousemove = function () {
     var xlcd = caidan.getElementsByTagName("ul")[0];
-    xlcd.style.display="block";
+    xlcd.style.display = "block";
     var j = caidan;
-    j.style.display="block";
+    j.style.display = "block";
 }
-bbfl.onmouseout=function(){
+bbfl.onmouseout = function () {
     var xlcd = caidan.getElementsByTagName("ul")[0];
-    xlcd.style.display="none";
+    xlcd.style.display = "none";
     var j = caidan;
-    j.style.display="none";
+    j.style.display = "none";
 }
 
-superior.onmousemove=function(){
+superior.onmousemove = function () {
     var xlcd = caidan.getElementsByTagName("ul")[0];
-    xlcd.style.display="block";
+    xlcd.style.display = "block";
     var j = caidan;
-    j.style.display="block";
+    j.style.display = "block";
 }
-superior.onmouseout=function(){
+superior.onmouseout = function () {
     var xlcd = caidan.getElementsByTagName("ul")[0];
-    xlcd.style.display="none";
+    xlcd.style.display = "none";
     var j = caidan;
-    j.style.display="none";
+    j.style.display = "none";
 }
 
 //限时抢购
 
 var xsqg = document.getElementById("centre_top_centre_g_id2");
 var superior2 = document.getElementById("centre_top_centre_2");
-var caidan2 =document.getElementById("centre_top_centre_1_cd2");
+var caidan2 = document.getElementById("centre_top_centre_1_cd2");
 
-xsqg.onmousemove=function(){
+xsqg.onmousemove = function () {
     var cd = caidan2.getElementsByTagName("ul")[0];
-    cd.style.display="block";
+    cd.style.display = "block";
     var g = caidan2;
-    g.style.display="block"
+    g.style.display = "block"
 }
-xsqg.onmouseout=function(){
+xsqg.onmouseout = function () {
     var cd = caidan2.getElementsByTagName("ul")[0];
-    cd.style.display="none";
+    cd.style.display = "none";
     var g = caidan2;
-    g.style.display="none"
+    g.style.display = "none"
 }
-superior2.onmousemove=function(){
+superior2.onmousemove = function () {
     var cd = caidan2.getElementsByTagName("ul")[0];
-    cd.style.display="block";
+    cd.style.display = "block";
     var g = caidan2;
-    g.style.display="block"
+    g.style.display = "block"
 }
-superior2.onmouseout=function(){
+superior2.onmouseout = function () {
     var cd = caidan2.getElementsByTagName("ul")[0];
-    cd.style.display="none";
+    cd.style.display = "none";
     var g = caidan2;
-    g.style.display="none"
+    g.style.display = "none"
 }
 
 //休闲团购
 var xxtg = document.getElementById("centre_top_centre_g_id3");
 var superior3 = document.getElementById("centre_top_centre_3");
-var caidan3 =document.getElementById("centre_top_centre_1_cd3");
+var caidan3 = document.getElementById("centre_top_centre_1_cd3");
 
-xxtg.onmousemove=function(){
+xxtg.onmousemove = function () {
     var cd3 = caidan3.getElementsByTagName("ul")[0];
-    cd3.style.display="block";
+    cd3.style.display = "block";
     var g3 = caidan3;
-    g3.style.display="block"
+    g3.style.display = "block"
 }
-xxtg.onmouseout=function(){
+xxtg.onmouseout = function () {
     var cd3 = caidan3.getElementsByTagName("ul")[0];
-    cd3.style.display="none";
+    cd3.style.display = "none";
     var g3 = caidan3;
-    g3.style.display="none"
+    g3.style.display = "none"
 }
-superior3.onmousemove=function(){
+superior3.onmousemove = function () {
     var cd3 = caidan3.getElementsByTagName("ul")[0];
-    cd3.style.display="block";
+    cd3.style.display = "block";
     var g3 = caidan3;
-    g3.style.display="block"
+    g3.style.display = "block"
 }
-superior3.onmouseout=function(){
+superior3.onmouseout = function () {
     var cd3 = caidan3.getElementsByTagName("ul")[0];
-    cd3.style.display="none";
+    cd3.style.display = "none";
     var g3 = caidan3;
-    g3.style.display="none"
+    g3.style.display = "none"
 }
 
 //1元疯抢
 
 var fq = document.getElementById("centre_top_centre_g_id4");
 var superior4 = document.getElementById("centre_top_centre_4");
-var caidan4 =document.getElementById("centre_top_centre_1_cd4");
+var caidan4 = document.getElementById("centre_top_centre_1_cd4");
 
-fq.onmousemove=function(){
+fq.onmousemove = function () {
     var cd4 = caidan4.getElementsByTagName("ul")[0];
-    cd4.style.display="block";
+    cd4.style.display = "block";
     var g4 = caidan4;
-    g4.style.display="block"
+    g4.style.display = "block"
 }
-fq.onmouseout=function(){
+fq.onmouseout = function () {
     var cd4 = caidan4.getElementsByTagName("ul")[0];
-    cd4.style.display="none";
+    cd4.style.display = "none";
     var g4 = caidan4;
-    g4.style.display="none"
+    g4.style.display = "none"
 }
-superior4.onmousemove=function(){
+superior4.onmousemove = function () {
     var cd4 = caidan4.getElementsByTagName("ul")[0];
-    cd4.style.display="block";
+    cd4.style.display = "block";
     var g4 = caidan4;
-    g4.style.display="block"
+    g4.style.display = "block"
 }
-superior4.onmouseout=function(){
+superior4.onmouseout = function () {
     var cd4 = caidan4.getElementsByTagName("ul")[0];
-    cd4.style.display="none";
+    cd4.style.display = "none";
     var g4 = caidan4;
-    g4.style.display="none"
+    g4.style.display = "none"
 }
 
 //满减平淡
 
 var mjpd = document.getElementById("centre_top_centre_g_id5");
 var superior5 = document.getElementById("centre_top_centre_5");
-var caidan5 =document.getElementById("centre_top_centre_1_cd5");
+var caidan5 = document.getElementById("centre_top_centre_1_cd5");
 
-mjpd.onmousemove=function(){
+mjpd.onmousemove = function () {
     var cd5 = caidan5.getElementsByTagName("ul")[0];
-    cd5.style.display="block";
+    cd5.style.display = "block";
     var g5 = caidan5;
-    g5.style.display="block"
+    g5.style.display = "block"
 }
-mjpd.onmouseout=function(){
+mjpd.onmouseout = function () {
     var cd5 = caidan5.getElementsByTagName("ul")[0];
-    cd5.style.display="none";
+    cd5.style.display = "none";
     var g5 = caidan5;
-    g5.style.display="none"
+    g5.style.display = "none"
 }
-superior5.onmousemove=function(){
+superior5.onmousemove = function () {
     var cd5 = caidan5.getElementsByTagName("ul")[0];
-    cd5.style.display="block";
+    cd5.style.display = "block";
     var g5 = caidan5;
-    g5.style.display="block"
+    g5.style.display = "block"
 }
-superior5.onmouseout=function(){
+superior5.onmouseout = function () {
     var cd5 = caidan5.getElementsByTagName("ul")[0];
-    cd5.style.display="none";
+    cd5.style.display = "none";
     var g5 = caidan5;
-    g5.style.display="none"
+    g5.style.display = "none"
 }
 
 //购物券领取
 
-var gwj3 =document.getElementById("gwj3");
-var gwj10 =document.getElementById("gwj10");
-var gwj120 =document.getElementById("gwj20");
-gwj3.onclick=function(){
+var gwj3 = document.getElementById("gwj3");
+var gwj10 = document.getElementById("gwj10");
+var gwj120 = document.getElementById("gwj20");
+gwj3.onclick = function () {
     alert("领取成功");
 }
-gwj10.onclick=function(){
+gwj10.onclick = function () {
     alert("领取成功");
 }
-gwj20.onclick=function(){
+gwj20.onclick = function () {
     alert("购物券以发完");
 }
+
+
+//搜索查找
+
+var sousuo = document.getElementById("sousuo");
+var ssl = document.getElementById("ssl")
+var jpms = document.getElementsByClassName("jpms");
+var xsqg = document.getElementsByClassName("xsqg");
+
+var url = "http://127.0.0.1:3000/";
+
+sousuo.onclick = function () {
+    $.post(url + 'select', {
+        ssl: ssl.value
+    }, function (data, status) {
+       
+        //console.log(jpms);
+        //坚果
+        for(var i=0; i<jpms.length; i++){
+            (function(i){
+                jpms[i].src =data[i].tupian;
+            }(i))
+        }
+
+        //jpms[0].src = data[0].tupian;
+        //干果
+        for(var i=0; i<xsqg.length;i++){
+            (function(i){
+                xsqg[i].src=data[i].tupian;
+            }(i))
+        }
+
+        if (data.result) {
+            alert("查询失败");
+            console.log(data);
+
+            //location.href="./project/ss/ss.html"
+
+        } else {
+            alert("查询成功");
+            console.log(data);
+        }
+    });
+}
+
--- routes/index.js
+++ routes/index.js
@@ -3,107 +3,128 @@ var router = express.Router();
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-  host: '127.0.0.1',
-  user: 'root',
-  database: 'zhouyujiang'
+    host: '127.0.0.1',
+    user: 'root',
+    database: 'zhouyujiang'
 });
 connection.connect(function (err) {
-  if (err) {
-    console.error('error connecting: ' + err.stack);
-    return;
-  }
+    if (err) {
+        console.error('error connecting: ' + err.stack);
+        return;
+    }
 });
-
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
 });
 
 module.exports = router;
 
-router.post('/login', function (req, res) {
-  res.contentType('json');
-  var params = {
-    username: req.body.username,
-    userpwd: req.body.userpwd
-  };
-
-  action_login(params.username, params.userpwd, function (result, id, name) {
-    params.result = result;
-    params.userid = id;
-    params.username = name;
-    console.log(params);
-    res.send(JSON.stringify(params));//给客户端返回一个json格式的数据
-    res.end();
-  });
-});
 
 //登录
 
+router.post('/login', function (req, res) {
+    res.contentType('json');
+    var params = {
+        username: req.body.username,
+        userpwd: req.body.userpwd
+    };
+
+    action_login(params.username, params.userpwd, function (result, id, name) {
+        params.result = result;
+        params.userid = id;
+        params.username = name;
+        console.log(params);
+        res.send(JSON.stringify(params));//给客户端返回一个json格式的数据
+        res.end();
+    });
+});
+
+
 function action_login(name, pwd, callback) {
-  var sqlCmd;
-  sqlCmd = 'SELECT * from users where UserName="' + name + '" or Tel="' + name + '" ';
-  //console.log(sqlCmd);
-  connection.query(sqlCmd, function (error, rows, fields) {
-    if (rows.length) {
-      //根据用户名和密码查找到匹配的数据
-      var userid = rows[0].Id;
-      var username = rows[0].UserName;
-      //connection.query('insert into cart (UserId) values ("' + userid + '")', function (error, rows, fields) {
-      //    console.log('rows')
-      //});
-      callback(true, userid, username);
-      console.log(rows[0]);
-    } else {
-      //没有匹配的数据
-      callback(false);
-    }
-  });
+    var sqlCmd;
+    sqlCmd = 'SELECT * from users where UserName="' + name + '" or Tel="' + name + '" ';
+    //console.log(sqlCmd);
+    connection.query(sqlCmd, function (error, rows, fields) {
+        if (rows.length) {
+            //根据用户名和密码查找到匹配的数据
+            var userid = rows[0].Id;
+            var username = rows[0].UserName;
+            //connection.query('insert into cart (UserId) values ("' + userid + '")', function (error, rows, fields) {
+            //    console.log('rows')
+            //});
+            callback(true, userid, username);
+            console.log(rows[0]);
+        } else {
+            //没有匹配的数据
+            callback(false);
+
+        }
+    });
 }
+
 
 //注册
 
 router.post('/reg', function (req, res) {
-  res.contentType('json');
-  var params = {
-    Tel: req.body.tel,
-    username: req.body.username,
-    UserPwd: req.body.password,
-  };
-  console.log(params);
-
-  action_reg(params.Tel, params.username, params.UserPwd,function (result, id) {
-    params.result = result;
-    params.userid = id;
+    res.contentType('json');
+    var params = {
+        Tel: req.body.tel,
+        username: req.body.username,
+        UserPwd: req.body.password,
+    };
     console.log(params);
-    res.send(JSON.stringify(params));//给客户端返回一个json格式的数据
-    res.end();
-  });
+
+    action_reg(params.Tel, params.username, params.UserPwd, function (result, id) {
+        params.result = result;
+        params.userid = id;
+        console.log(params);
+        res.send(JSON.stringify(params));//给客户端返回一个json格式的数据
+        res.end();
+    });
 });
 
 function action_reg(tel, name, pwd, callback) {
-  var sqlCmd;
-  sqlCmd = 'SELECT * from users where UserName="' + name + '" or Tel="' + tel + '"';
-  connection.query(sqlCmd, function (error, rows, fields) {
-    console.log(rows);
-    if (!rows.length) {
-      //当前用户未注册
-      var insertCmd = 'insert into users (Tel,UserName,UserPwd) values ("' + tel + '","' + name + '","' + pwd + '")';
-      connection.query(insertCmd, function () {
-        connection.query(sqlCmd, function (error, rows, fields) {
-          console.log(rows[0]);
-          var userid = rows[0].Id;
-          callback(true, userid);
-        })
-      });
-    } else {
-      //当前用户已经注册
-      callback(false);
-    }
-  });
+    var sqlCmd;
+    sqlCmd = 'SELECT * from users where UserName="' + name + '" or Tel="' + tel + '"';
+    connection.query(sqlCmd, function (error, rows, fields) {
+        console.log(rows);
+        if (!rows.length) {
+            //当前用户未注册
+            var insertCmd = 'insert into users (Tel,UserName,UserPwd) values ("' + tel + '","' + name + '","' + pwd + '")';
+            connection.query(insertCmd, function () {
+                connection.query(sqlCmd, function (error, rows, fields) {
+                    console.log(rows[0]);
+                    var userid = rows[0].Id;
+                    callback(true, userid);
+                })
+            });
+        } else {
+            //当前用户已经注册
+            callback(false);
+        }
+    });
 }
 
+//搜索
 
+router.post('/select', function (req, res) {
+    console.log(req.body)
+    res.contentType('json');
+
+        var sqlCmd;
+        sqlCmd = 'SELECT * from shangping where ID="' + req.body.ssl + '" or lei="' + req.body.ssl + '"';
+        //console.log(sqlCmd);
+        connection.query(sqlCmd, function (error, rows, fields) {
+            if (rows.length) {
+                res.send(JSON.stringify(rows));// /给客户端返回一个json格式的数据
+               res.end();
+            } else {
+                callback(false);
+            }
+        });
+
+});
 
